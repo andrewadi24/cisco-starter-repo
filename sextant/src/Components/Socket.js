6ffd1eefac25e60c29319ae0b4a783853f506ae8
andrew <andrew.adisaputra@rocketmail.com>
2023-04-05 16:13:55 +1000

    simplified code

--- sextant/src/Components/Socket.js
+++ sextant/src/Components/Socket.js
@@ -2,15 +2,8 @@ import React, { useState, useCallback, useEffect } from 'react';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
 
 const Socket = () => {
-    const [socketUrl, setSocketUrl] = useState('ws://localhost:55455');
-    const [messageHistory, setMessageHistory] = useState([]);
-    const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
-    const now = Date.now()
-    useEffect(() => {
-        if (lastMessage !== null) {
-            setMessageHistory((prev) => prev.concat(lastMessage));
-        }
-    }, [lastMessage, setMessageHistory]);
+    const socketUrl = "ws://localhost:55455"
+    const { _, lastMessage, _1 } = useWebSocket(socketUrl);
     return (
         <div>{lastMessage ? <span>{Date.now() - lastMessage.data} miliseconds</span> : null}</div>
     )
